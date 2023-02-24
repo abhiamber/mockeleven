d56d355a782c4d7b617f2a8d7841e6f42834f8f2
abhiamber191 <abhiamber191@gmail.com>
2023-02-24 23:59:11 +0530

    check

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get("/", async (req, res) => {
   res.send("HUrray !");
 });
 
+app.get("/hello", async (req, res) => {
+  res.send("HUrray !");
+});
 app.listen(8080, () => {
   console.log("server working");
 });
